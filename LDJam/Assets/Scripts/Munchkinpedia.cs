38581125a38b9db1def54a3be43512a9e38892b3
Tyler Wong <tywong8@gmail.com>
2024-10-06 16:19:52 -0400

    blub

--- LDJam/Assets/Scripts/Munchkinpedia.cs
+++ LDJam/Assets/Scripts/Munchkinpedia.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Munchkinpedia : MonoBehaviour
 {
+    [SerializeField] GameObject munchkinpediaObj;
+    bool toggleMunchkinpedia;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        munchkinpediaObj.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            toggleMunchkinpedia = !toggleMunchkinpedia;
+        }
+
+        if (toggleMunchkinpedia)
+        {
+            munchkinpediaObj.SetActive(true);
+        }
+        else if (!toggleMunchkinpedia)
+        {
+            munchkinpediaObj.SetActive(false);
+        }
     }
 }
